7744b6e1eb5a668289500694552d72eac9b381bf
Henry Wang <wanghenry79@gmail.com>
2018-03-31 09:44:33 +0800

    Created Playground and finished part 1

---
+++ SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,31 @@
+/* Part 1: */
+// Player Information stored in Dictionary
+let player1: [String:String] = ["Name":"Joe Smith", "Height":"42 inches", "Experience":"Yes", "Guardian Names":"Jim and Jan Smith"]
+let player2: [String:String] = ["Name":"Jill Tanner", "Height":"36 inches", "Experience": "Yes", "Guardian Names":"Clara Tanner"]
+let player3: [String:String] = ["Name":"Bill Bon", "Height":"43 inches", "Experience":"Yes", "Guardian Names":"Sara and Jenny Bon"]
+let player4: [String:String] = ["Name":"Eva Gordon", "Height":"45 inches", "Experience":"No", "Guardian Names":"Wendy and Mike Gordon"]
+let player5: [String:String] = ["Name":"Matt Gill", "Height":"40 inches", "Experience":"No", "Guardian Names":"Charles and Sylvia Gill"]
+let player6: [String:String] = ["Name":"Kimmy Stein", "Height":"41 inches", "Experience":"No", "Guardian Names":"Bill and Hillary Stein"]
+let player7: [String:String] = ["Name":"Sammy Adams", "Height":"45 inches", "Experience":"No", "Guardian Names":"Jeff Adams"]
+let player8: [String:String] = ["Name":"Karl Saygan", "Height":"42 inches", "Experience":"Yes", "Guardian Names":"Heather Bledsoe"]
+let player9: [String:String] = ["Name":"Suzane Greenberg", "Height":"44 inches", "Experience":"Yes", "Guardian Names":"Henrietta Dumas"]
+let player10: [String:String] = ["Name":"Sal Dali", "Height":"41 inches", "Experience":"No", "Guardian Names":"Gala Dali"]
+let player11: [String:String] = ["Name":"Joe Kavalier", "Height":"39 inches", "Experience":"No", "Guardian Names":"Sam and Elaine Kavalier"]
+let player12: [String:String] = ["Name":"Ben Finkelstein", "Height":"44 inches", "Experience":"No", "Guardian Names":"Aaron and Jill Finkelstein"]
+let player13: [String:String] = ["Name":"Diego Soto", "Height":"41 inches", "Experience":"Yes", "Guardian Names":"Robin and Sarika Soto"]
+let player14: [String:String] = ["Name":"Chloe Alaska", "Height":"47 inches", "Experience":"No", "Guardian Names":"David and Jamie Alaska"]
+let player15: [String:String] = ["Name":"Arnold Willis", "Height":"43 inches", "Experience":"No", "Guardian Names":"Claire Willis"]
+let player16: [String:String] = ["Name":"Phillip Helm", "Height":"44 inches", "Experience":"Yes", "Guardian Names":"Thomas Helm and Eva Jones"]
+let player17: [String:String] = ["Name":"Les Clay", "Height":"42 inches", "Experience":"Yes", "Guardian Names":"Wynonna Brown"]
+let player18: [String:String] = ["Name":"Herschel Krustofski", "Height":"45 inches", "Experience":"Yes", "Guardian Names":"Hyman and Rachel Krustofski"]
+
+// Array of Total Players
+let allPlayers: [[String:String]] = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+
+/* Part 2: */
+
+
+/* Part 3: */
+
+
+
