4a9b15f02a4271dde45a7137176ad6a82a4d9fd8
Henry Wang <wanghenry79@gmail.com>
2018-04-03 18:11:14 -0700

    Part 2 Finished
    Finished Part 2's logic on sorting players to either an experienced or non-experienced group and logic on sorting them to can equal number of groups. Will be working on sorting them based on height soon.

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,3 +1,6 @@
+/* Soccer Coordinator */
+/* Note that most of these comments were made in order to give me a confirmation of the constant/variable types */
+
 /* Part 1: */
 // Player Information stored in Dictionary
 let player1: [String:Any] = ["Name":"Joe Smith", "Height":42, "Experience":true, "Guardian Names":"Jim and Jan Smith"]
@@ -20,21 +23,71 @@ let player17: [String:Any] = ["Name":"Les Clay", "Height":42, "Experience":true,
 let player18: [String:Any] = ["Name":"Herschel Krustofski", "Height":45, "Experience":true, "Guardian Names":"Hyman and Rachel Krustofski"]
 
 // Information of All Players
-let allPlayers: [[String:Any]] = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+let players: [[String:Any]] = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18] // players is an array with dictionaries
 
-var players: [String:Bool] = ["Joe Smith":true,"Jill Tanner":true,"Bill Bon":true,"Eva Gordon":false,"Matt Gill":false,"Kimmy Stein":false,"Sammy Adams":false,"Karl Saygan":true,"Suzane Greenberg":true,"Sal Dali":false,"Joe Kavalier":false,"Ben Finkelstein":false,"Diego Soto":true,"Chloe Alaska":false,"Arnold Willis":false,"Phillip Helm":true,"Les Clay":true,"Herschel Krustofski":true]
+// Experience Levels of All Players
+let experienceLevels: [String:Bool] = ["Joe Smith":true,"Jill Tanner":true,"Bill Bon":true,"Eva Gordon":false,"Matt Gill":false,"Kimmy Stein":false,"Sammy Adams":false,"Karl Saygan":true,"Suzane Greenberg":true,"Sal Dali":false,"Joe Kavalier":false,"Ben Finkelstein":false,"Diego Soto":true,"Chloe Alaska":false,"Arnold Willis":false,"Phillip Helm":true,"Les Clay":true,"Herschel Krustofski":true]
 
 /* Part 2: */
-var experiencedPlayers: [String] = []
+// Variable Declaration
+var experiencedPlayers = [[String:Any]]() // experiencedPlayers is an array with dictionaries
+var nonExperiencedPlayers = [[String:Any]]() //nonExperiencedPlayers is an array with dictionaries
+var teamDragons = [String]() // teamDragons is an array with dictionaries
+var teamSharks = [String]() // teamSharks is an array with dictionaries
+var teamRaptors = [String]() // teamRaptors is an array with dictionaries
+
 for player in players {
-    if let isExperienced = player["isExperienced"] as? Bool {
-        //Do some stuff here'
-        experiencedPlayers.append()
+    if player["Experience"] as? Bool == true { // Player is a dictionary constant from players array
+        experiencedPlayers.append(player) // the player dictionary is being appended to experiencedPlayers which is a array with dictionaries
     } else {
-        //Do some other stuff here
+        nonExperiencedPlayers.append(player) // the player dictionary is being appended to the nonExperiencedPlayers which is an array with dictionaries
+    }
 }
 
+// Constant theKey is the name of the player and constant theValue is either true or false
+// For loop assigns the players to either an experienced group or a non-experienced group
+/* for (theKey,theValue) in experienceLevels {
+    if theValue == true {
+        experiencedPlayers.append(theKey) // Appends the person's name to a list (array) of experienced players
+    } else {
+        nonExperiencedPlayers.append(theKey) // Appends the person's name to a list (array) of non-experienced players
+    }
+} */
+// Prints out experiencedPlayers and nonExperiencedPlayers
+// print(experiencedPlayers)
+// print(nonExperiencedPlayers)
+
+
+// Function sortToTeam allows computer to do its best in assigning the players to equal teams
+func sortToTeam(forGroup group: [[String:Any]]) {
+    for player in group { // group is either experiencedPlayers or nonExperiencedPlayers
+        if teamSharks.count <= teamDragons.count && teamSharks.count <= teamRaptors.count {
+            teamSharks.append(player["Name"] as! String)
+        } else if teamDragons.count <= teamRaptors.count && teamDragons.count <= teamSharks.count {
+            teamDragons.append(player["Name"] as! String)
+        } else if teamRaptors.count <= teamSharks.count && teamRaptors.count <= teamDragons.count {
+            teamRaptors.append(player["Name"] as! String)
+        }
+    }
+
+}
+sortToTeam(forGroup: experiencedPlayers)
+sortToTeam(forGroup: nonExperiencedPlayers)
+
+// Printing the players on each team (the array form is still curr
+print(teamSharks)
+/* Experimenting with Function
+func teamSorter(player name: String, isExperienced experienceLevel: Bool, height: Int) -> Void {
+    if experienceLevel == true {
+        experiencedPlayers.append(name)
+    } else {
+        nonExperiencedPlayers.append(name)
+    }
+ 
+}*/
+
 /* Part 3: */
+
 
 
 
